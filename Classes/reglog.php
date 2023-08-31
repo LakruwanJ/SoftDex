c4c1e8b62cf4fb83176f934b3e14713b15f18292
Lakruwan Jayathissa <lakruwan987@gmail.com>
2023-09-01 03:54:10 +0530

    sign in password entering function

--- Classes/reglog.php
+++ Classes/reglog.php
@@ -15,28 +15,27 @@ require 'DbConnector.php';
 
 use Classes\DbConnector;
 
-
 $reglog_ = new reglog();
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
+
     if (isset($_POST["signIn"])) {
         if (!empty($_POST["username"]) && !empty($_POST["password"])) {
-            if ($_POST["password"] == $reglog_->login($_POST["password"])) {
-                session_start(); 
+            $reglog_->reglog2($_POST["username"]);
+            if ($_POST["password"] == $reglog_->login() ){
+                session_start();
                 $_SESSION["user"] = $_POST["username"];
-                header("Location: ../Softdex.php");                
-            }else{
-                header("Location: ../index.php?e=1&mode=signup");
-            }           
-        }else{
+                header("Location: ../Softdex.php");
+            } else {
+                header("Location: ../index.php?e=1&mode=signin");
+            }
+        } else {
             header("Location: ../index.php?e=2");
         }
     } else {
         header("Location: ../index.php?e=3");
     }
-    
+
 //    if (isset($_POST["signUp"])) {
 //        if (!empty($_POST["username"]) && !empty($_POST["password"])) {
 //            if ($_POST["password"] == $reglog_->login($username)) {
@@ -58,16 +57,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 class reglog {
 
-    public function login($username) {
+    private $username;
+    private $pw_;
+    private $email;
+
+    function reglog1($username, $pw, $email) {
+        $this->username = $username;
+        $this->pw = $pw;
+        $this->email = $email;
+    }
+
+    function reglog2($username) {
+        $this->username = $username;
+    }
+
+    public function login() {
+        $pww = null;
         $dbcon = new DbConnector();
         $con = $dbcon->getConnection();
         $query = "SELECT password FROM user WHERE username = ?;";
         $pstmt = $con->prepare($query);
-        $pstmt->bindValue(1, $username);
-        $pw = $pstmt->execute();
-        echo $query;
-        echo $pw;
-        return $pw;
+        $pstmt->bindValue(1, $this->username);
+        $pstmt->execute();
+        $pw = $pstmt->fetchAll(PDO::FETCH_OBJ);
+        foreach ($pw as $value) {
+            $pww = $value->password;
+        }
+        return $pww;
     }
 
 }
--- index.php
+++ index.php
@@ -3,9 +3,19 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="css/reglog.css" />
         <title>Sign in & Sign up Form</title>
+
+        <style>
+            body {
+                margin: 0;
+                padding: 0;
+                overflow-x: hidden; /* This hides the horizontal scrollbar */
+            }
+
+        </style>
     </head>
     <body>
         <div class="container">
@@ -39,25 +49,79 @@
                         </table>
 
                         <input type="submit" value="Login" class="btn solid" name="signIn"/>
-
+                        <br><br>
                     </form>
 
-                    <form action="#" class="sign-up-form">
+                    <form action="#" class="sign-up-form needs-validation" novalidate>
                         <img src="img/logo3.png" alt="logo" style="height:60px;"><br><br>
                         <h2 class="title">Sign up</h2>
                         <div class="input-field">
                             <i class="fas fa-user"></i>
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" required/>
                         </div>
                         <div class="input-field">
                             <i class="fas fa-envelope"></i>
-                            <input type="email" placeholder="Email" />
+                            <input type="email" placeholder="Email" required />
                         </div>
                         <div class="input-field">
                             <i class="fas fa-lock"></i>
-                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Password" name="pWord" id="pWord" required/>
                         </div>
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" placeholder="Re-enter Password" name="pWord2" id="pWord2" required/>
+                        </div>
+
+                        <p id="password-error" style="color: red;"></p>
+                        <script>
+                            const passwordInput = document.getElementById("pWord");
+                            const passwordError = document.getElementById("password-error");
+
+                            passwordInput.addEventListener("input", function () {
+                                const password = passwordInput.value;
+
+                                const hasLowerCase = /[a-z]/.test(password);
+                                const hasUpperCase = /[A-Z]/.test(password);
+                                const hasDigit = /\d/.test(password);
+                                const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\-]/.test(password);
+                                const isLengthValid = password.length >= 8 && password.length <= 20;
+
+                                if (!isLengthValid) {
+                                    passwordError.textContent = "Password must be between 8 and 20 characters.";
+                                } else if (!hasLowerCase) {
+                                    passwordError.textContent = "Password must contain at least one lowercase letter.";
+                                } else if (!hasUpperCase) {
+                                    passwordError.textContent = "Password must contain at least one uppercase letter.";
+                                } else if (!hasDigit) {
+                                    passwordError.textContent = "Password must contain at least one digit.";
+                                } else if (!hasSpecialChar) {
+                                    passwordError.textContent = "Password must contain at least one special character.";
+                                } else {
+                                    passwordError.textContent = "";
+                                }
+
+                                const reenteredPasswordInput = document.getElementById("pWord2");
+                                const signUpButton = document.querySelector(".btn");
+
+                                function checkPasswordMatch() {
+                                    const password = passwordInput.value;
+                                    const reenteredPassword = reenteredPasswordInput.value;
+
+                                    if (password === reenteredPassword) {
+                                        passwordError.textContent = ""; // Clear password match error
+                                        signUpButton.disabled = !validatePassword(); // Enable/disable based on password validity
+                                    } else {
+                                        passwordError.textContent = "Passwords do not match.";
+                                        signUpButton.disabled = true; // Disable the button if passwords don't match
+                                    }
+                                }
+
+                                reenteredPasswordInput.addEventListener("input", checkPasswordMatch);
+
+
+                            });</script>
                         <input type="submit" class="btn" value="Sign up" />
+                        <br><br>
                     </form>
                 </div>
             </div>
@@ -101,6 +165,6 @@
             });
         </script>
         <script src="js/reglog.js"></script>
-
+        <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     </body>
 </html>
