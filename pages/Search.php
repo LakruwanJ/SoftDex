06ea1ab540f23567ea42cc30e8b83c37e5c0dc19
Lakruwan Jayathissa <lakruwan987@gmail.com>
2023-09-01 14:19:42 +0530

    edit

--- pages/Search.php
+++ pages/Search.php
@@ -50,7 +50,7 @@ $star = [254, 20, 6, 15, 63, 150];
         <!----------------------------------------------------------- nav bar start ---------------------------------------------------------->
         <nav class="navbar navbar-expand-lg navbar-dark bg-color fixed-top">
             <div class="container">
-                <a class="navbar-brand logog" href="#"><img src="../img/logo.png" alt="logo" style="height:50px;"></a>
+                <a class="navbar-brand logog" href="#"><img src="img/logo.png" alt="logo" style="height:50px;"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -61,16 +61,32 @@ $star = [254, 20, 6, 15, 63, 150];
                                     class="fa-solid fa-house  icoon"></i>Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link_  active" href="#"><i class="fa-solid fa-book icoon"></i>Categories</a>
+                            <a class="nav-link nav-link_  " href="#"><i class="fa-solid fa-book icoon"></i>Categories</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link nav-link_  active" href="#"><i class="fa-regular fa-clock icoon"></i>Comming soon</a>
+                            <a class="nav-link nav-link_  " href="#"><i class="fa-regular fa-clock icoon"></i>Comming soon</a>
+                        </li>
+<!--                        without login
+                        <li class="nav-item ">
+                            <a class="nav-link " href="#"><i class="fa-solid fa-right-to-bracket icoon"></i>Login</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link active " href="#"><i class="fa-solid fa-right-to-bracket icoon"></i>Login</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link active " href="#"><button class="btn btn-primary" type="button">Sign UP</button></a>
+                            <a class="nav-link " href="#"><button class="btn btn-primary" type="button">Sign UP</button></a>
+
+                        </li>-->
+                        <!--with login-->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="img/user (2).png" width="40" height="40" class="rounded-circle">
+                            </a>
+                            <ul class="dropdown-menu">
+                                <center><li><?php echo $user;?></li></center>
+                                <hr class="p-0 m-0 mt-1">
+                                <li><a class="dropdown-item " href="#"><i class="fas fa-user mr-2 icoon"></i>Account</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-arrow-trend-up icoon"></i>Become a Developer</a></li>
+                                <hr class="p-0 m-0 mt-1">
+                                <li><a class="dropdown-item" href="#"><i class="fa-solid fa-right-from-bracket icoon"></i>Logout</a></li>
+                            </ul>
 
                         </li>
                     </ul>
