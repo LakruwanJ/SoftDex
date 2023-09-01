d8c5ad6691048384fe859e75c4c2c2863a11e7da
Hashinisulakshana <hashinisulakshanakumari@gmail.com>
2023-09-01 21:19:26 +0530

    update pages
    aboutus and cart page

--- pages/Aboutus.php
+++ pages/Aboutus.php
@@ -14,24 +14,22 @@
         <div class="card">
             <img src="../img/download.png" alt="Benefit 1">
             <h3>Software Downloading</h3>
-            <p> Enhance your digital experience with our vast collection of software downloads. Whether you are a student, a professional, or a tech enthusiast, we have a diverse range of software solutions carefully curated to cater to your specific requirements. You can explore our extensive collection of both free and premium software options, all guaranteed to deliver top-notch performance and reliability. Say goodbye to the hassle of searching multiple sources; find all your desired software at SoftDex.
-                Embrace innovation and creativity as we collaborate to shape a digital landscape that caters to your individual needs and aspirations. Together, we build a stronger, smarter, and more connected world through the power of software.</p>
-            <button class="see-more-btn">See More</button>
+            
+
+            <p>"Enhance your digital experience with our diverse software collection. Whether you're a student, professional, or tech enthusiast, explore our free and premium options for top performance. Find all your desired software at SoftDex. Join us to shape a smarter, more connected world through software.We build a stronger, smarter, and more connected world through the power of software."</p>
         </div>
 
         <div class="card">
             <img src="../img/upload.jpg" alt="Benefit 2">
             <h3>Software Uploading</h3>
-            <p>we provide a reliable and user-friendly platform for uploading software applications.Are you a software developer, a software company, or a part of an open-source project looking to share your creations with the world? SoftDex provides you with a reliable and easy-to-use platform to upload your software applications. Simply create an account, submit your software, and reach a broader audience.Embrace innovation and creativity as we collaborate to shape a digital landscape that caters to your individual needs and aspirations.  We value innovation and are committed to supporting developers in showcasing their talents and creations.we build a more connected world through the power of software.</p>
-        <button class="see-more-btn">See More</button>
+            <p>"We offer a reliable, user-friendly platform for software uploads. Whether you're a software developer, company, or part of an open-source project, SoftDex simplifies sharing. Create an account, submit your software, and reach a wider audience. Join us in shaping a digital landscape that meets your unique needs. We value innovation, supporting developers, and building a more connected world through software."</p>
+        
         </div>
 
         <div class="card">
             <img src="../img/custom.png" alt="Benefit 3">
             <h3>Create customized Software</h3>
-            <p>At SoftDex, we understand that not every off-the-shelf software can meet your unique needs. That's why we offer you the opportunity to create customized software solutions that align precisely with your requirements. Define your software specifications, including desired features, functionalities, and user interface elements. Moreover, our platform allows you to explore developer profiles, giving you the freedom to select a preferred software developer to bring your vision to life. Let your imagination soar, and we will help you transform your ideas into reality.
-Since user can Let your imagination soar, and we will help you transform your ideas into reality. </p>
-       <button class="see-more-btn">See More</button>
+            <p>"At SoftDex, we understand that not every off-the-shelf software can meet your unique needs. That's why we offer you the opportunity to create customized software solutions that align precisely with your requirements. Define your software specifications, including desired features, functionalities, and user interface elements.Let your imagination soar, and we will help you transform your ideas into reality."</p>
         </div>
         
     </div>
@@ -41,3 +39,4 @@ Since user can Let your imagination soar, and we will help you transform your id
 
 </body>
 </html>
+
