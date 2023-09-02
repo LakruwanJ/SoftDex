642e7019b4b62bab0ca50b8403b433f9829ede3d
Lakruwan Jayathissa <lakruwan987@gmail.com>
2023-09-02 16:11:22 +0530

    fix errors

--- Classes/cartCls.php
+++ Classes/cartCls.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -16,58 +16,61 @@ require 'DbConnector.php';
 use Classes\DbConnector;
 
 $cartObj = new cartCls();
+$text = NULL;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (isset($_POST["addtocart"])) {
 
         if (!empty($_POST["user"]) && !empty($_POST["sw"])) {
-            
+
             $cartObj->cartCls($_POST["user"], $_POST["sw"]);
             $temp = $cartObj->getcart();
             $cartObj->additemtocart($temp);
-            
         }
-        
     }
-    
 }
 
-
-
 class cartCls {
-    
-    private $userid;
+
+    private $user;
     private $softwareid;
-    
-    public function cartCls($userid, $softwareid) {
-        $this->userid = $userid;
+
+    public function cartCls($user, $softwareid) {
+        $this->user = $user;
         $this->softwareid = $softwareid;
     }
-    
-     public function additemtocart($text) {
-        $dbcon = new DbConnector();
-        $con = $dbcon->getConnection();
-         $sql = "UPDATE user SET cart=? WHERE Uid=?";
-         $newtext = $text." | ".$this->softwareid;
-            $pstmt = $con->prepare($sql);
-            $pstmt->bindValue(1, $newtext);
-            $pstmt->bindValue(2, $this->userid);
-            $pstmt->execute();
 
-            if ($pstmt->rowCount() > 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-    }
-    
-     public function getcart() {
+    public function additemtocart($text) {
         $dbcon = new DbConnector();
         $con = $dbcon->getConnection();
-        $query = "SELECT cart FROM user WHERE Uid=?";
+        $sql = "UPDATE user SET cart=? WHERE username=?";
+        if ($text === null) {
+            $newtext = $this->softwareid;
+        } else {
+            $newtext = $text . " | " . $this->softwareid;
+        }
+        $pstmt = $con->prepare($sql);
+        $pstmt->bindValue(1, $newtext);
+        $pstmt->bindValue(2, $this->user);
+        echo $newtext;
+        echo $this->user;
+        echo $sql;
+        $pstmt->execute();
+
+        if ($pstmt->rowCount() > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function getcart() {
+        $dbcon = new DbConnector();
+        $con = $dbcon->getConnection();
+        $query = "SELECT cart FROM user WHERE username=?";
         $pstmt = $con->prepare($query);
-        $pstmt->bindValue(1, $this->userid);
+        $pstmt->bindValue(1, $this->user);
         $pstmt->execute();
         $item = $pstmt->fetchAll(PDO::FETCH_OBJ);
         foreach ($item as $value) {
@@ -75,8 +78,5 @@ class cartCls {
         }
         return $text;
     }
-    
 
-    
 }
-
