444b3f8a8805a00864424b58031a023f14276a1b
ThakshilaDulanjani <thakshilaweerasingha2@gmail.com>
2023-11-06 23:46:23 +0530

    comingsoon
    comigsoon

---
+++ pages/coimgswregister.php
@@ -0,0 +1,41 @@
+<?php
+require_once '../Classes/DbConnector.php';
+require_once '../Classes/Comingsoonsw.php';
+
+use Classes\DbConnector;
+use Classes\Comingsoonsw;
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $devname = $_POST["devname"];
+        $swname = $_POST["swname"];
+        $details = $_POST["details"];
+        $dateofrelease = $_POST["dateofrelease"];
+
+
+        if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            if (isset($_POST['submit'])) {
+                if (empty($_POST['devname']) || empty($_POST['swname']) || empty($_POST['details']) || empty($_POST['dateofrelease'])) {
+                    header("Location:comingsoon.php?error=3");
+                } else {
+                    $devname = $_POST["devname"];
+                    $swname = $_POST["swname"];
+                    $details = $_POST["details"];
+                    $dateofrelease = $_POST["dateofrelease"];
+                    $comingsoonswregister = new Comingsoonsw($devname, $swname, $details, $dateofrelease);
+                    $comingsoonswregister->comingsoonswregister();
+                }
+            } else {
+                header("Location:comingsoon.php?error=2");
+            }
+        } else {
+            header("Location:comingsoon.php?error=1");
+        }
+        ?>
+    </body>
+</html>
